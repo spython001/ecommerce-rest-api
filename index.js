7cc7e1b6d1f43665ffa2ab56132e69d0fd6da352
spython <emmanuelegbo01@gmail.com>
2023-10-08 10:30:26 +0100

    modified files

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const { dot } = require("node:test/reporters");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => {console.error(err);});
 
 app.listen(5000, ()=>{
     console.log("Backend server is running")
