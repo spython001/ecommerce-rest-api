4cbb56fad4ea8a6ca3245c48e1cbf7b210de0259
spython <emmanuelegbo01@gmail.com>
2023-10-08 15:52:51 +0100

    modified contents of product js route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 //ROUTES
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running")
