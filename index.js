2b239cbd4b127618a704ff83e7953ddb1c53d7a6
spython <emmanuelegbo01@gmail.com>
2023-10-08 11:17:41 +0100

    modified files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -12,6 +13,12 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => {console.error(err);});
 
-app.listen(5000, ()=>{
+//MIDDLEWARES
+app.use(express.json());
+
+//ROUTES
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running")
 });
